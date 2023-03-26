f1f49a64bfd676eb5cac6e999c6186624181f869
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-03-26 20:04:08 -0300

    ✨ Create and finish clean data component 03

---
+++ components/03_basic_clean/basic_clean.py
@@ -0,0 +1,55 @@
+'''
+This .py file is used to clean up the data,
+for example removing outliers.
+
+Author: Vitor Abdo
+Date: March/2023
+'''
+
+# import necessary packages
+import logging
+import pandas as pd
+import wandb
+
+# basic logs config
+logging.basicConfig(
+    level=logging.INFO,
+    filemode='w',
+    format='%(asctime)-15s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+
+
+def clean_data() -> None:
+    '''Function to clean up our training dataset to feed the machine
+    learning model.
+    '''
+    # start a new run at wandb
+    run = wandb.init(
+        project='risk_assessment',
+        entity='vitorabdo',
+        job_type='clean_data')
+    artifact = run.use_artifact('vitorabdo/risk_assessment/train_set.csv:latest', type='dataset')
+    filepath = artifact.file()
+    logger.info('Downloaded trusted data artifact: SUCCESS')
+
+    # clean the train dataset
+    df_trusted = pd.read_csv(filepath)
+    df_clean = df_trusted.copy()
+    logger.info('Train dataset are clean: SUCCESS')
+
+    # upload to W&B
+    artifact = wandb.Artifact(
+        name='clean_data',
+        type='dataset',
+        description='Clean dataset after we apply "clean_data" function')
+
+    df_clean.to_csv('df_clean.csv', index=False)
+    artifact.add_file('df_clean.csv')
+    run.log_artifact(artifact)
+    logger.info('Artifact Uploaded: SUCCESS')
+
+
+if __name__ == "__main__":
+    logging.info('About to start executing the clean_data function')
+    clean_data()
+    logging.info('Done executing the clean_data function')
