06b8ea89eb50f03b135c76b6fc542e42dece1ecb
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-03-27 15:38:56 -0300

    🎨 Finish unit tests for check the data

--- components/04_data_check/conftest.py
+++ components/04_data_check/conftest.py
@@ -6,26 +6,49 @@ Date: March/2023
 '''
 
 # import necessary packages
+import sys
 import pytest
 import pandas as pd
 import wandb
 
+# config
+DATA = sys.argv[1]
+REF_DATA = sys.argv[2]
+
 
 @pytest.fixture(scope='session')
-def data():
-    '''fixture to generate data to our tests'''
+def data(DATA):
+    '''Fixture to generate data to our tests'''
     run = wandb.init(
         project='risk_assessment',
         entity='vitorabdo',
         job_type='data_tests',
         resume=True)
 
-    # Download input artifact. This will also note that this script is using this
-    # particular version of the artifact
-    data_path = run.use_artifact('vitorabdo/risk_assessment/clean_data:latest').file()
+    # download input artifact
+    data_path = run.use_artifact(DATA).file()
 
     if data_path is None:
         pytest.fail('You must provide the csv file')
 
     df = pd.read_csv(data_path)
     return df
+
+
+@pytest.fixture(scope='session')
+def ref_data(REF_DATA):
+    '''Fixture to generate the reference data for non-deterministic tests'''
+    run = wandb.init(
+        project='risk_assessment',
+        entity='vitorabdo',
+        job_type='data_tests',
+        resume=True)
+
+    # download input artifact
+    data_path = run.use_artifact(REF_DATA).file()
+
+    if data_path is None:
+        pytest.fail('You must provide the csv file')
+
+    ref_df = pd.read_csv(data_path)
+    return ref_df
--- components/04_data_check/test_data.py
+++ components/04_data_check/test_data.py
@@ -6,15 +6,91 @@ Author: Vitor Abdo
 Date: March/2023
 '''
 
+# import necessary packages
+import pandas as pd
+import scipy.stats
+
+# DETERMINISTIC TESTS
+
+def test_import_data(data):
+    '''Test that the dataset is not empty'''
+
+    assert data.shape[0] > 0
+    assert data.shape[1] > 0
+
+
 def test_column_names(data):
     '''Tests if the column names are the same as the original
     file, including in the same order
     '''
     expected_colums = [
-        'corporation', 'lastmonth_activity', 'lastyear_activity', 
+        'corporation', 'lastmonth_activity', 'lastyear_activity',
         'number_of_employees', 'exited']
 
     these_columns = data.columns.values
 
     # This also enforces the same order
     assert list(expected_colums) == list(these_columns)
+
+
+def test_entries_values(data):
+    '''Test dataset variable entries'''
+
+    # independent variables
+    assert all(data.lastmonth_activity.values) >= 0
+    assert all(data.lastyear_activity.values) >= 0
+    assert all(data.number_of_employees.values) >= 0
+
+    # label
+    known_label_entries = [0, 1]
+    label_column = set(data.exited.unique())
+    assert set(known_label_entries) == set(label_column)
+
+
+# NON DETERMINISTIC TESTS
+
+def test_similar_label_distrib(
+        data: pd.DataFrame, ref_data: pd.DataFrame):
+    '''Apply a threshold on the KL divergence to detect if the distribution of the new
+    data is significantly different than that of the reference dataset
+    '''
+    dist1 = data['exited'].value_counts().sort_index()
+    dist2 = ref_data['exited'].value_counts().sort_index()
+
+    assert scipy.stats.entropy(dist1, dist2, base=2) < 0.20
+
+
+def apply_lastmonth_activity_ttest(
+        data: pd.DataFrame, ref_data: pd.DataFrame):
+    '''Tests whether the means of two independent samples are significantly different'''
+
+    dist1 = data['lastmonth_activity'].values
+    dist2 = ref_data['lastmonth_activity'].values
+
+    ts, pvalues = scipy.stats.ttest_ind(dist1, dist2, axis=1)
+
+    assert pvalues > 0.05
+
+
+def apply_lastyear_activity_ttest(
+        data: pd.DataFrame, ref_data: pd.DataFrame):
+    '''Tests whether the means of two independent samples are significantly different'''
+
+    dist1 = data['lastyear_activity'].values
+    dist2 = ref_data['lastyear_activity'].values
+
+    ts, pvalues = scipy.stats.ttest_ind(dist1, dist2, axis=1)
+
+    assert pvalues > 0.05
+
+
+def apply_number_of_employees_ttest(
+        data: pd.DataFrame, ref_data: pd.DataFrame):
+    '''Tests whether the means of two independent samples are significantly different'''
+
+    dist1 = data['number_of_employees'].values
+    dist2 = ref_data['number_of_employees'].values
+
+    ts, pvalues = scipy.stats.ttest_ind(dist1, dist2, axis=1)
+
+    assert pvalues > 0.05
