625d5e6127673069c1a065d7201c7a72966fbdce
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-03-29 17:56:24 -0300

    ✅ Add unit tests for API model inference

---
+++ tests/test_local_api.py
@@ -0,0 +1,63 @@
+'''
+Unit test of ml_api.py API module with pytest
+
+Author: Vitor Abdo
+Date: March/2023
+'''
+
+# import necessary packages
+import json
+import logging
+from fastapi.testclient import TestClient
+from ml_api import app
+
+logging.basicConfig(
+    level=logging.INFO,
+    filemode='w',
+    format='%(asctime)-15s - %(name)s - %(levelname)s - %(message)s')
+
+# create a test client instance
+client = TestClient(app)
+
+
+def test_get():
+    '''Test welcome message for get at root'''
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == 'Welcome to our model API'
+
+
+def test_inference_class1():
+    '''Test model inference output for class 1'''
+    sample = {
+        "corporation": "nciw",
+        "lastmonth_activity": 45, 
+        "lastyear_activity": 0,
+        "number_of_employees": 99
+    }
+
+    data = json.dumps(sample)
+
+    response = client.post('/risk_assessment_prediction', data=data)
+
+    # test response and output
+    assert response.status_code == 200
+    assert response.json() == 'The person is at risk of leaving the company'
+
+
+def test_inference_class0():
+    '''Test model inference output for class 0'''
+    sample =  {
+        "corporation": "lsid",
+        "lastmonth_activity": 36, 
+        "lastyear_activity": 234,
+        "number_of_employees": 541
+    }
+
+    data = json.dumps(sample)
+
+    response = client.post('/risk_assessment_prediction', data=data)
+
+    # test response and output
+    assert response.status_code == 200
+    assert response.json() == 'The person has no risk of leaving the company'
