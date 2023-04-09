610e37df3cd3e7d1f5d6933e82f8f199622accb9
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-04-09 11:42:11 -0300

    ⚡️ Create a scheduler to orchestrate system

---
+++ scheduler.py
@@ -0,0 +1,30 @@
+'''
+Scheduler creation to orchestrate the system
+
+Author: Vitor Abdo
+Date: April/2023
+'''
+
+# import necessary packages
+from apscheduler.schedulers.blocking import BlockingScheduler
+import subprocess
+
+def run_mlflow():
+    subprocess.run(['mlflow', 'run', '.', '-P', 'steps=upload_raw_data,upload_trusted_data'])
+
+def run_diagnostics():
+    subprocess.Popen('python model_data_diagnostics/data_drift_check.py')
+    subprocess.Popen('python model_data_diagnostics/model_drift_check.py')
+
+def run_mlflow_if_model_drift():
+    with open('model_drift_check_result.txt', 'r') as f:
+        result = f.read().strip()
+        if result == 'True':
+            subprocess.run(['mlflow', 'run', '.'])
+
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+    scheduler.add_job(run_mlflow, 'cron', hour=3)
+    scheduler.add_job(run_diagnostics, 'cron', hour=4)
+    scheduler.add_job(run_mlflow_if_model_drift, 'cron', hour=5)
+    scheduler.start()
